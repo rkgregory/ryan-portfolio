d2d0e7fbc3c6ac86b42e6a35a59f57828706086f
Ryan <ryan@debian-machine.kbradydavis.com>
2020-09-01 21:25:23 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>RYAN WUZ HERE LOL</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          hehaw hello world
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
